fdc2b67269903c5b0749b50345ef58096690cc53
Jason Ruesch <jruesch@degreed.com>
2024-06-06 11:23:24 -0400

    test(visualizer): fix broken tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 /// <reference types='vitest' />
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: __dirname,
@@ -35,9 +35,6 @@ export default defineConfig({
 
   test: {
     globals: true,
-    cache: {
-      dir: './node_modules/.vitest/visualizer',
-    },
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
 
@@ -46,5 +43,7 @@ export default defineConfig({
       reportsDirectory: './coverage/visualizer',
       provider: 'v8',
     },
+
+    passWithNoTests: true,
   },
 });
